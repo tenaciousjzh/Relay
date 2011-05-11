62ec060ee112d4b9bb1fd318684049bb52060982
Jared Holmberg <jared.holmberg@gmail.com>
2011-05-11 00:07:28 -0600

    Found out the reason for the error was that the static method being called was a variadic java method where it expected something like (ChannelHandler ...) as the argument. Used (into-array) to get the call from clojure on teh variadic method to work correctly. Output the raw bytes returned as chars.

--- relay-web/src/main/clojure/org/relay/app.clj
+++ relay-web/src/main/clojure/org/relay/app.clj
@@ -1,5 +1,8 @@
 (ns org.relay
+  (:use (org.relay.http.handlers [discard-server-handler]))
   (:gen-class))
 
 (defn -main [& args]
-  (println "This is the main function for app."))
+  (println "This is the main function for app.")
+  (run-discard-server 8080)
+  )
--- relay-web/src/main/clojure/org/relay/http/handlers/discard_server_handler.clj
+++ relay-web/src/main/clojure/org/relay/http/handlers/discard_server_handler.clj
@@ -2,21 +2,20 @@
   (:import
     (org.jboss.netty.bootstrap ServerBootstrap)
     (org.jboss.netty.buffer ChannelBuffer)
-    (org.jboss.netty.channel SimpleChannelHandler ChannelPipelineFactory Channels)
+    (org.jboss.netty.channel SimpleChannelHandler ChannelPipelineFactory Channels ChannelHandler)
     (org.jboss.netty.channel.socket.nio NioServerSocketChannelFactory)
     (java.net InetSocketAddress)
     (java.util.concurrent Executors)
     )
   )
 
-
-
-(def discard-server-handler
+(def discard-handler
   (proxy [SimpleChannelHandler] []
     (messageReceived [channel-handler-context event]
       (let [buf (cast ChannelBuffer (. event getMessage))]
+        (println "Discarding message: ")
         (while (. buf readable)
-            (println (cast char (. buf readByte)))
+            (print (char (. buf readByte)))
           )))
     (exceptionCaught [channel-handler-context exception-event]
       (println "Discard Server ran into a problem processing a request: " exception-event)
@@ -25,9 +24,15 @@
 (def channel-pipeline-factory
   (reify ChannelPipelineFactory
     (getPipeline [this]
-      (Channels/pipeline discard-server-handler))))
+      ;had trouble passing in discard-handler to Channel/pipeline because it was a variadic function
+      ;e.g - it expected ChannelHandler... variadic argument. StackOverFlow had a great post on this:
+      ;http://stackoverflow.com/questions/5638541/problems-calling-a-variadic-java-function-from-clojure
+      (Channels/pipeline (into-array ChannelHandler [discard-handler])))))
 
-(defn run-discard-server [port]
+(defn run-discard-server
+  "Generates a ChannelHandler that will discard messages when they are received. This is a
+  contrived example mostly to determine how Clojure's interop facilities will work with Netty."
+  [port]
   (let [factory (NioServerSocketChannelFactory. (Executors/newCachedThreadPool) (Executors/newCachedThreadPool))
         bootstrap (ServerBootstrap. factory)
         _ (. bootstrap (setPipelineFactory channel-pipeline-factory))
